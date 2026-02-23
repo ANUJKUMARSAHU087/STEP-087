7cd55d966dae1f1211feb20e30c42f64013aa6b4
ANUJKUMARSAHU087 <as3336@srmist.edu.in>
2026-02-23 11:01:02 +0530

    UC7: Added new functionality

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 public class PalindromeChecker {
     public static void main(String[] args) {
+
 
                 Scanner sc = new Scanner(System.in);
 
@@ -9,27 +12,32 @@ public class PalindromeChecker {
                 String str = sc.nextLine();
 
                 Stack<Character> stack = new Stack<>();
+                Queue<Character> queue = new LinkedList<>();
 
-                // Push all characters into stack
+                // Insert into both
                 for(int i = 0; i < str.length(); i++) {
-                    stack.push(str.charAt(i));
+                    stack.push(str.charAt(i));   // LIFO
+                    queue.add(str.charAt(i));    // FIFO
                 }
 
-                String reversed = "";
-
-                // Pop characters (reverses automatically)
-                while(!stack.isEmpty()) {
-                    reversed = reversed + stack.pop();
-                }
+                boolean isPalindrome = true;
 
                 // Compare
-                if(str.equals(reversed)) {
-                    System.out.println(str + " is a Palindrome");
-                } else {
-                    System.out.println(str + " is NOT a Palindrome");
+                while(!stack.isEmpty()) {
+                    if(stack.pop() != queue.remove()) {
+                        isPalindrome = false;
+                        break;
+                    }
                 }
 
+                if(isPalindrome)
+                    System.out.println(str + " is a Palindrome");
+                else
+                    System.out.println(str + " is NOT a Palindrome");
+
                 sc.close();
+
+
 
     }
 }
