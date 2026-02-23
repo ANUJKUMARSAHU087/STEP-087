3569bb80ea55d75a466ba44e065b42abcf2da8b3
ANUJKUMARSAHU087 <as3336@srmist.edu.in>
2026-02-23 10:26:17 +0530

    Added new code in U4

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
 public class PalindromeChecker {
     public static void main(String[] args) {
-        System.out.println("WELCOME");
 
+        String str = "madam";   // hardcoded value
+        String rev = "";
+
+        // reverse the string
+        for(int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+
+        // check palindrome
+        if(str.equals(rev)) {
+            System.out.println(str + " is a Palindrome");
+        } else {
+            System.out.println(str + " is NOT a Palindrome");
+        }
     }
 }
